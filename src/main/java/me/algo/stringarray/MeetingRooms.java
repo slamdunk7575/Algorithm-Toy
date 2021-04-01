7c1b185aeba0537c3b307d36f69d3ed5910bccd1
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-02 02:38:09 +0900

    (feat) MeetingRooms 해결

---
+++ src/main/java/me/algo/stringarray/MeetingRooms.java
@@ -0,0 +1,49 @@
+package me.algo.stringarray;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+
+public class MeetingRooms {
+
+    public boolean solve(Interval[] intervals) {
+
+        if (Objects.isNull(intervals)) {
+            return false;
+        }
+
+        Arrays.sort(intervals, comparator);
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i-1].end > intervals[i].start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    Comparator<Interval> comparator = new Comparator<Interval>() {
+        @Override
+        public int compare(Interval o1, Interval o2) {
+            // start 기준 오름차순
+            return o1.start - o2.start;
+        }
+    };
+
+}
+
+class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        this.start = 0;
+        this.end =0;
+    }
+
+    Interval(int s, int e) {
+        this.start = s;
+        this.end = e;
+    }
+}
---
+++ src/test/java/me/algo/stringarray/MeetingRoomsTest.java
@@ -0,0 +1,32 @@
+package me.algo.stringarray;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MeetingRoomsTest {
+
+    @DisplayName("MeetingRooms 테스트")
+    @Test
+    void meeting_rooms_test() {
+        // given
+        MeetingRooms meetingRooms = new MeetingRooms();
+
+        /*Interval interval1 = new Interval(15, 20);
+        Interval interval2 = new Interval(5, 10);
+        Interval interval3 = new Interval(0, 30);*/
+
+        Interval interval1 = new Interval(7, 10);
+        Interval interval2 = new Interval(2, 4);
+
+        Interval[] intervals = {interval1, interval2};
+
+        // when
+        boolean result = meetingRooms.solve(intervals);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+}
