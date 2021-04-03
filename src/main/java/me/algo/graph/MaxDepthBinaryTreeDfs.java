74637168aabc1438562afbfd39b18b60425f3657
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-03 11:26:19 +0900

    (feat) MaxDepthBinaryTree DFS 해결

---
+++ src/main/java/me/algo/graph/MaxDepthBinaryTreeDfs.java
@@ -0,0 +1,49 @@
+package me.algo.graph;
+
+import java.util.Objects;
+import java.util.Stack;
+
+public class MaxDepthBinaryTreeDfs {
+
+    public int solve(TreeNode root) {
+
+        if (Objects.isNull(root)) {
+            return 0;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> levelStack = new Stack<>();
+        stack.push(root);
+        levelStack.push(1);
+        int max = 0;
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int count = levelStack.pop();
+            max = Math.max(max, count);
+
+            if (node.left != null) {
+                stack.push(node.left);
+                levelStack.push(count + 1);
+            }
+
+            if (node.right != null) {
+                stack.push(node.right);
+                levelStack.push(count + 1);
+            }
+
+        }
+
+        return max;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+        this.val = x;
+    }
+}
