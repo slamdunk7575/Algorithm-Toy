733ab6bad92d04c9faf5b4e13adb45d8330f3542
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-17 00:03:30 +0900

    (feat) TwoSum 해결

---
+++ src/test/java/me/algo/stringarray/TwoSumTest.java
@@ -0,0 +1,34 @@
+package me.algo.stringarray;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TwoSumTest {
+
+
+    @Test
+    @DisplayName("TwoSum 테스트")
+    void two_sum_test() {
+        // given
+        TwoSum twoSum = new TwoSum();
+        int[] nums = { 2, 8, 11, 14 };
+        int target = 16;
+
+        // when
+        int[] result = twoSum.solve(nums, target);
+
+        // then
+        assertThat(result).containsExactly(1, 3);
+
+        String collect = Arrays.stream(result)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(", "));
+        System.out.println(collect);
+    }
+
+}
