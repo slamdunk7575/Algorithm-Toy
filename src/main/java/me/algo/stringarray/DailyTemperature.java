4ff10efe7d12a8ce2b12ad22a4aac40bfd3afe48
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-17 21:31:57 +0900

    (feat) DailyTemperature 해결

---
+++ src/main/java/me/algo/stringarray/DailyTemperature.java
@@ -0,0 +1,24 @@
+package me.algo.stringarray;
+
+import java.util.Stack;
+
+public class DailyTemperature {
+
+        public int[] solve(int[] temper) {
+            // 1. 담을 그릇 정하기
+            Stack<Integer> stack = new Stack<>();
+            int[] result = new int[temper.length];
+
+            // 2. for 문
+            for (int i = 0; i < temper.length; i++) {
+                System.out.println("Temper: " + temper[i]);
+                while (!stack.isEmpty() && temper[stack.peek()] < temper[i]) {
+                    int index = stack.pop();
+                    System.out.println("i: " + i + ", index: " + index);
+                    result[index] = i - index;
+                }
+                stack.push(i);
+            }
+            return result;
+        }
+}
---
+++ src/test/java/me/algo/stringarray/DailyTemperatureTest.java
@@ -0,0 +1,26 @@
+package me.algo.stringarray;
+
+import me.algo.util.PrintUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DailyTemperatureTest {
+
+    @Test
+    @DisplayName("DailyTemperature 테스트")
+    void daily_temperature_test() {
+        // given
+        DailyTemperature dailyTemperature = new DailyTemperature();
+        int[] nums = { 73, 74, 75, 71, 69, 72, 76, 73 };
+
+        // when
+        int[] result = dailyTemperature.solve(nums);
+
+        // then
+        PrintUtil. print(result);
+        assertThat(result).containsExactly(1, 1, 4, 2, 1, 1, 0, 0);
+    }
+
+}
