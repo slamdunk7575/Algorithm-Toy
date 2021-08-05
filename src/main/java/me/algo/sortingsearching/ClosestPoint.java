8ce91aba42f9e9ccd911f5b89c8f150ecbdd7ece
slamdunk7575 <slamdunk7575@gmail.com>
2021-08-05 17:17:27 +0900

    (feat) 원점에서 가장 가까운 지점 (K Closest Points to Origin) 해결

---
+++ src/main/java/me/algo/sortingsearching/ClosestPoint.java
@@ -0,0 +1,60 @@
+package me.algo.sortingsearching;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * (정리)
+ * 1.
+ *
+ * 시간복잡도: O(NlogN)
+ * - 대상: int[][] points
+ * - 이유: 이차원 배열의 갯수 n * 우선순위 큐 logN
+ *
+ * 공간복잡도: O(k)
+ * - 대상: 새로운 결과 저장공간 (int[][] result = new int[k][2])
+ * - 이유: 우선순위 큐 maxHeap에서 최상단 k개 만큼만 유지하기 때문
+ */
+public class ClosestPoint {
+
+    public int[][] solve(int[][] points, int k) {
+        // Data Structure
+        Queue<int[]> pq = new PriorityQueue<>((b, a) ->
+                (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1]));
+
+        for (int[] point : points) {
+            pq.offer(point);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        // 결과
+        int[][] result = new int[k][2];
+
+        int index = 0;
+        while (index < k) {
+            result[index] = pq.poll();
+            index++;
+        }
+
+        return result;
+    }
+
+    public void print(int[][] result) {
+        Arrays.stream(result)
+                .flatMapToInt(arr -> Arrays.stream(arr))
+                .forEach(System.out::println);
+
+        /*int m = result.length;
+        int n = result[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("[" + i + "]" + "[" + j + "]: " + result[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+    }
+}
