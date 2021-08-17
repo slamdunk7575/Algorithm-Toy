3a36a5c906cd7bb75fe8135ce366cc5dd935fe56
slamdunk7575 <slamdunk7575@gmail.com>
2021-08-17 19:27:08 +0900

    (feat) 아나그램 만들기 해결

---
+++ src/test/java/me/algo/array/MakingAnagramsTest.java
@@ -0,0 +1,25 @@
+package me.algo.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MakingAnagramsTest {
+
+    @DisplayName("아나그램 만들기")
+    @Test
+    void making_anagrams() {
+        // given
+        String first = "cde";
+        String second = "abc";
+        MakingAnagrams makingAnagrams = new MakingAnagrams();
+
+        // when
+        int result = makingAnagrams.solve(first, second);
+
+        // then
+        assertThat(result).isEqualTo(4);
+    }
+
+}
