5efc1d37a35976e310d0acdb92cbc471aa912d23
slamdunk7575 <slamdunk7575@gmail.com>
2021-08-22 15:27:14 +0900

    (feat) Minimum Size Subarray Sum 풀이

---
+++ src/test/java/me/algo/array/MinSizeSubArrayTest.java
@@ -0,0 +1,25 @@
+package me.algo.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MinSizeSubArrayTest {
+
+    @DisplayName("서브 Array 합이 조건을 만족하는 가장 작은 사이즈 구하기")
+    @Test
+    void minimum_size_sub_array() {
+        // given
+        int target = 7;
+        int[] nums = { 2, 3, 1, 2, 4, 3 };
+        MinSizeSubArray minSizeSubArray = new MinSizeSubArray();
+
+        // when
+        int result = minSizeSubArray.solve(target, nums);
+
+        // then
+        assertThat(result).isEqualTo(2);
+    }
+
+}
