8e58d266261d4dc90c3d33d7de1c544970a67a5a
slamdunk7575 <slamdunk7575@gmail.com>
2021-08-23 13:03:45 +0900

    (feat) TrappingRainWater(빗물 담기) 해결

---
+++ src/test/java/me/algo/array/TrappingRainWaterTest.java
@@ -0,0 +1,24 @@
+package me.algo.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TrappingRainWaterTest {
+
+    @DisplayName("빗물담기")
+    @Test
+    void trapping_rain_water() {
+        // given
+        int[] nums = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        TrappingRainWater trw = new TrappingRainWater();
+
+        // when
+        int result = trw.solve(nums);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+}
