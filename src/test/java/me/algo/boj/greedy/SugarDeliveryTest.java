27d5f446d698059e5f007d53a33efa6179f4b97c
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-20 22:19:40 +0900

    (feat) BOJ 설탕 배달

---
+++ src/test/java/me/algo/boj/greedy/SugarDeliveryTest.java
@@ -0,0 +1,24 @@
+package me.algo.boj.greedy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SugarDeliveryTest {
+
+    @DisplayName("설탕 배달 #2839")
+    @Test
+    void sugarDelivery() {
+        // given
+        int input = 11;
+        SugarDelivery sugarDelivery = new SugarDelivery();
+
+        // when
+        int result = sugarDelivery.solve(input);
+
+        // then
+        assertThat(result).isEqualTo(3);
+    }
+
+}
