0ee92f895e9cb40a33b9d0daae93db0373da23ff
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-22 21:38:10 +0900

    (feat) BOJ 잃어버린 괄호

---
+++ src/main/java/me/algo/boj/greedy/LostParenthesis.java
@@ -0,0 +1,32 @@
+package me.algo.boj.greedy;
+
+/**
+ * (정리)
+ * '+'을 먼저 계산하고(=괄호로 묶고) '-'를 계산한다
+ */
+public class LostParenthesis {
+
+    public int solve(String input) {
+
+        String[] numsBySub = input.split("-");
+        int result = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numsBySub.length; i++) {
+
+            String[] numsByAdd = numsBySub[i].split("\\+");
+
+            int sum = 0;
+            for (int j = 0; j < numsByAdd.length; j++) {
+                sum += Integer.parseInt(numsByAdd[j]);
+            }
+
+            if (result == Integer.MAX_VALUE) {
+                result = sum;
+            } else {
+                result -= sum;
+            }
+        }
+
+        return result;
+    }
+}
