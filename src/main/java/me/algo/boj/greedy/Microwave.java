6e261316e579d9daae077a3e6a760b0f7ac0d900
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-23 15:40:22 +0900

    (feat) BOJ 전자레인지

---
+++ src/main/java/me/algo/boj/greedy/Microwave.java
@@ -0,0 +1,22 @@
+package me.algo.boj.greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Microwave {
+
+    private List<Integer> times = Arrays.asList(5 * 60, 1 * 60, 10);
+
+    public List<Integer> solve(int input) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int time : times) {
+            result.add(input / time);
+            input = input % time;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/me/algo/boj/greedy/MicrowaveTest.java
@@ -0,0 +1,26 @@
+package me.algo.boj.greedy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MicrowaveTest {
+
+    @DisplayName("전자레인지 #10162")
+    @Test
+    void microwave() {
+        // given
+        int cookingTime = 100;
+        Microwave microwave = new Microwave();
+
+        // when
+        List<Integer> result = microwave.solve(cookingTime);
+
+        // then
+        assertThat(result).containsAll(Arrays.asList(0, 1, 4));
+    }
+}
