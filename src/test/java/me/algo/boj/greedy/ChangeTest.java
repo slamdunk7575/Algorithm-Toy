545313f08a8009acbba7890ebbaafe0a3bcd4670
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-23 12:39:27 +0900

    (feat) BOJ 거스름돈

---
+++ src/test/java/me/algo/boj/greedy/ChangeTest.java
@@ -0,0 +1,23 @@
+package me.algo.boj.greedy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChangeTest {
+
+    @DisplayName("거스름돈 #5585")
+    @Test
+    void change() {
+        // given
+        int money = 380;
+        Change change = new Change();
+
+        // when
+        int result = change.solve(money);
+
+        // then
+        assertThat(result).isEqualTo(4);
+    }
+}
