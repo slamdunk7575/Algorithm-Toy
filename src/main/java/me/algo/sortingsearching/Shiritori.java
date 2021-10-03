8f26c3950d6cb68aa76ad5a10809d796107ffe81
slamdunk7575 <slamdunk7575@gmail.com>
2021-10-03 12:13:39 +0900

    (feat) 끝말잇기

---
+++ src/main/java/me/algo/sortingsearching/Shiritori.java
@@ -0,0 +1,31 @@
+package me.algo.sortingsearching;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shiritori {
+
+    public int[] solution(int n, String[] words) {
+        int[] result = new int[2];
+        List<String> wordBasket = new ArrayList<>();
+        boolean canGameRunning = true;
+
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0 && words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)
+                    || wordBasket.contains(words[i])) {
+                result[0] = (i % n) + 1;
+                result[1] = (i / n) + 1;
+                canGameRunning = false;
+                break;
+            }
+
+            wordBasket.add(words[i]);
+        }
+
+        if (canGameRunning) {
+            return new int[]{0, 0};
+        }
+
+        return result;
+    }
+}
