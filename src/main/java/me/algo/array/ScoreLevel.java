0846d367cd0539ad10e2111e4e3adcc880189799
slamdunk7575 <slamdunk7575@gmail.com>
2022-02-10 05:05:52 +0900

    (feat) 점수에 등수 부여하기

---
+++ src/main/java/me/algo/array/ScoreLevel.java
@@ -0,0 +1,46 @@
+package me.algo.array;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ScoreLevel {
+
+    public int[] solution(int[] grades) {
+
+        List<Integer> result = new ArrayList<>();
+
+        List<Integer> sortedGrades = Arrays.stream(grades)
+                .mapToObj(Integer::valueOf)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        Map<Integer, Integer> ranks = new HashMap<>();
+
+        for (int i = 0; i < sortedGrades.size(); i++) {
+            Integer grade = sortedGrades.get(i);
+            ranks.put(grade, ranks.getOrDefault(grade, 0) + 1);
+        }
+
+        for (int grade : grades) {
+            // 나보다 높은 점수를 받은 사람수 + 1 = 내 등수
+            result.add(findHighGradeCount(ranks, grade) + 1);
+        }
+
+        return result.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+
+    /*
+     점수 - 사람수
+       3 - 1
+       2 - 2
+       1 - 1
+     */
+    private int findHighGradeCount(Map<Integer, Integer> ranks, int grade) {
+        return ranks.entrySet().stream()
+                .filter(entry -> entry.getKey() > grade)
+                .map(Map.Entry::getValue)
+                .reduce(0, Integer::sum);
+    }
+}
---
+++ src/test/java/me/algo/array/ScoreLevelTest.java
@@ -0,0 +1,24 @@
+package me.algo.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ScoreLevelTest {
+
+    @DisplayName("점수에 등수 부여하기")
+    @Test
+    void score_level() {
+        // given
+        int[] grade = {3, 2, 1, 2};
+        ScoreLevel scoreLevel = new ScoreLevel();
+
+        // when
+        int[] result = scoreLevel.solution(grade);
+
+        // then
+        assertThat(result).containsExactly(new int[]{1, 2, 4, 2});
+    }
+
+}
