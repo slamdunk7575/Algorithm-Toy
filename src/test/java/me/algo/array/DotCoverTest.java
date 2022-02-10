231cb150e9351e893611d1f33787f2edee963aa8
slamdunk7575 <slamdunk7575@gmail.com>
2022-02-11 03:01:18 +0900

    (feat) 선분으로 점들 덮기 (푸는중)

---
+++ src/test/java/me/algo/array/DotCoverTest.java
@@ -0,0 +1,25 @@
+package me.algo.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DotCoverTest {
+
+    @DisplayName("선분으로 점들 덮기")
+    @Test
+    void dot_cover() {
+        // given
+        int[] dots = {1, 5, 8};
+        int[] lines = {1, 3, 4, 6};
+        DotCover dotCover = new DotCover();
+
+        // when
+        int result = dotCover.solution(dots, lines);
+
+        // then
+        assertThat(result).isEqualTo(2);
+    }
+
+}
