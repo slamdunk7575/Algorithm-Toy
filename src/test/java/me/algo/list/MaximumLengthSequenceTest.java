f4dd31fd3762772829e120cdb21500fde793f1b7
slamdunk7575 <slamdunk7575@gmail.com>
2024-03-25 21:17:04 +0900

    (feat) 최대 길이 연속수열

---
+++ src/test/java/me/algo/list/MaximumLengthSequenceTest.java
@@ -0,0 +1,24 @@
+package me.algo.list;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaximumLengthSequenceTest {
+
+    @DisplayName("최대 길이 연속수열")
+    @Test
+    void data_structure_list() {
+        // given
+        MaximumLengthSequence maximumLengthSequence = new MaximumLengthSequence();
+        int[] input = new int[] { 8, 1, 9, 3, 10, 2, 4, 0, 2, 3 };
+
+        // when
+        int result = maximumLengthSequence.solution(input);
+
+        // then
+        assertThat(result).isEqualTo(5);
+    }
+
+}
