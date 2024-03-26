2f1ff7a03bcee54ec99bdf4250ff8e2a19d3d3e1
slamdunk7575 <slamdunk7575@gmail.com>
2024-03-26 20:46:23 +0900

    (feat) 이진수 정렬

---
+++ src/test/java/me/algo/sortingsearching/BinarySortingTest.java
@@ -0,0 +1,24 @@
+package me.algo.sortingsearching;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinarySortingTest {
+
+    @DisplayName("이진수 정렬")
+    @Test
+    void binary_sorting_test() {
+        // given
+        int[] inputs = { 5, 6, 7, 8, 9 };
+        BinarySorting binarySorting = new BinarySorting();
+
+        // when
+        int[] answers = binarySorting.solution(inputs);
+
+        // then
+        assertThat(answers[0]).isEqualTo(8);
+    }
+
+}
